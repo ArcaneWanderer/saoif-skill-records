7254352f464f0928f732acb0925446ee8bfc1747
ArcaneWanderer <arcanemindtraveler@gmail.com>
2019-10-30 23:24:33 +0800

    Added skill record backgrounds

--- js/script.js
+++ js/script.js
@@ -149,9 +149,18 @@ Vue.component('skill-record-info', {
                 </div>
                 <p class="card-title">{{ skillRecord.cardName }}</p>
                 <div class="card-rarity">
-                    <span v-for="index in skillRecord.cardInfo.rarity" :key="index">⭐</span>
+                    <span v-for="index in skillRecord.cardInfo.rarity" :key="index"><img width=25em height=25em src="http://localhost:5000/img/UI_icon_status_rare_on.png"></span>
+                    <span v-if="skillRecord.cardInfo.evolution_card_masterid > 0"><img width=25em height=25em src="http://localhost:5000/img/UI_icon_status_rare_off.png"></span>
                 </div>
                 <div class="card-img">
+                    <img v-if="skillRecord.cardInfo.rarity == 1 && skillRecord.cardInfo.evolution_card_masterid == 0" src="http://localhost:5000/img/Base_Frame/sr_base_1_2.png">
+                    <img v-else-if="skillRecord.cardInfo.rarity == 1" src="http://localhost:5000/img/Base_Frame/sr_base_1_1.png">
+                    <img v-if="skillRecord.cardInfo.rarity == 2 && skillRecord.cardInfo.evolution_card_masterid == 0" src="http://localhost:5000/img/Base_Frame/sr_base_2_2.png">
+                    <img v-else-if="skillRecord.cardInfo.rarity == 2" src="http://localhost:5000/img/Base_Frame/sr_base_2_1.png">
+                    <img v-if="skillRecord.cardInfo.rarity == 2 && skillRecord.cardInfo.evolution_card_masterid == 0" src="http://localhost:5000/img/Base_Frame/sr_base_3_2.png">
+                    <img v-else-if="skillRecord.cardInfo.rarity == 3" src="http://localhost:5000/img/Base_Frame/sr_base_3_1.png">
+                    <img v-if="skillRecord.cardInfo.rarity == 2 && skillRecord.cardInfo.evolution_card_masterid == 0" src="http://localhost:5000/img/Base_Frame/sr_base_4_2.png">
+                    <img v-else-if="skillRecord.cardInfo.rarity == 4" src="http://localhost:5000/img/Base_Frame/sr_base_4_1.png">
                     <img v-bind:src="'https://raw.githubusercontent.com/Nayuta-Kani/SAOIF-Skill-Records-Database/master/srimages/sr_icon_l_' + (skillRecord.cardInfo.evolution_card_masterid > 0 ? skillRecord.cardInfo.evolution_card_masterid : skillRecord.cardInfo.card_masterid) + '.png'">
                 </div>
                 <div class="card-details">

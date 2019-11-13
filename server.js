9c5b256bd5af5b6ef2e027b7d0f5ef641ba04a3d
ArcaneWanderer <arcanemindtraveler@gmail.com>
2019-11-14 01:07:14 +0800

    Create a basic Express server

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
+// const port = 5000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express');
+
+const app = express();
+const hostname = 'localhost';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));

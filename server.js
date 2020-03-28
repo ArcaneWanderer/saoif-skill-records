04e6da50e7394a926aae17ed3e9027ff7f8c8dcc
ArcaneWanderer <arcanemindtraveler@gmail.com>
2020-03-29 01:06:26 +0800

    Update Express routing for the language options

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const TEXT_DATABASE_TW = 'db/zh/textmaster.db3';
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+app.get('/:language', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
